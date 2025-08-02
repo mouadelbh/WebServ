33cee9782eb1d2a3de2d065a2c70e806c694372f
mel-bouh <mel-bouh@c3r5p10.1337.ma>
2025-08-02 11:33:38 +0100

    added post still need chuncked

--- src/main.cpp
+++ src/main.cpp
@@ -6,23 +6,50 @@
 /*   By: mel-bouh <mel-bouh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 16:21:14 by mel-bouh          #+#    #+#             */
-/*   Updated: 2025/07/10 14:25:09 by mel-bouh         ###   ########.fr       */
+/*   Updated: 2025/05/28 15:07:35 by mel-bouh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/include.hpp"
+#include "../Parse_Config/Parser.hpp"
 
 bool	run = true;
-bool 	autoIndex = false;
+bool 	autoIndex = true;
+const ServerConfig* g_current_server_config = nullptr;
 
-int main(int ac, char **av) {
-	if (ac != 1) {
-		std::cerr << "Usage: " << av[0] << std::endl;
+int main(int ac, char **av) 
+{
+	std::string config_path = "Parse_Config/default.conf";
+	if (ac == 2) config_path = av[1];
+
+	try 
+	{
+		Parser parser(config_path);
+		const std::vector<ServerConfig>& servers = parser.getServers();
+		if (servers.empty()) {
+			std::cerr << "No valid server config found!" << std::endl;
+			return 1;
+		}
+
+		std::cout << "Found " << servers.size() << " server configuration(s)" << std::endl;
+		
+		for (size_t i = 0; i < servers.size(); ++i) 
+		{
+			std::cout << "Server " << (i + 1) << ": " << servers[i].server_name 
+					  << " on " << servers[i].host << ":" << servers[i].port << std::endl;
+		}
+		
+		Server server;
+		std::unordered_map<int, Client> clients;
+		init(av);
+		g_current_server_config = &servers[0];
+		server.initServer(servers[0]);
+		server.runServer(clients);
+	}
+	catch (const std::exception& e) {
+		std::cerr << "Configuration error: " << e.what() << std::endl;
 		return 1;
 	}
-	Server server;
-	std::unordered_map<int, Client> clients;
-	init(av);
-	server.initServer();
-	server.runServer(clients);
+	
+	return 0;
 }
